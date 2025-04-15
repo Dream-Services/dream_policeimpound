0abc8d6fb76410adc12060dc9d14144d3049e034
Tuncion Helper <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-15 21:07:37 +0000

    chore: Updated manifest data for release 1.0.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,9 +6,9 @@ lua54 'yes'
 
 author 'Dream Services | Tuncion'
 description 'https://discord.gg/zppUXj4JRm'
-version '1.0.2'
-patch '#21'
-released '01.04.2025, 23:34 by Tuncion'
+version '1.0.3'
+patch '#25'
+released '15.04.2025, 23:07 by Tuncion'
 
 client_scripts {
     'bridge/**/client.lua',
